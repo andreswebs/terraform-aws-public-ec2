3963ec5d64c291839a42a83e300d2694349a3640
Andre Silva <andreswebs@pm.me>
2023-12-08 00:43:21 -0300

    allow web traffic

--- main.tf
+++ main.tf
@@ -30,6 +30,20 @@ resource "aws_security_group" "this" {
     cidr_blocks = var.cidr_whitelist
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
